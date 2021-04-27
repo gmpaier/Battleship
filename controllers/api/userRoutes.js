87c439afa9a80fd2f72b4443d9ebb2758fe84f06
Dgehring <dgehring7@gmail.com>
2021-04-26 20:13:50 -0400

    created login in userRoutes.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+router.post('/login', async (req, res) => {
+    try {
+        const validUsername = await User.findOne({where: {username: req.body.username}});
+
+        if (!validUsername) {
+            res.status(400).json({ message: 'Password or Username is incorrect, please try again'});
+            return;
+        }
+
+        const validPassword = await validUsername.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Password or Username is incorrect, please try again'});
+            return;
+        };
+
+        req.session.save(() => {
+            req.session.user_id = validUsername.id;
+            req.session.logged_in = true;
+            res.json({ user: validUsername, message: `Welcome ${validUsername}`})
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(200).end();
+        });
+    } else {
+        res.status(400).end
+    }
+});
+
+module.exports = router;
