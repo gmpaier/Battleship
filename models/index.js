888a09153eca1187f789439b1256a1086889c272
grif <gmpaier@loyola.edu>
2021-04-27 19:53:31 -0400

    greatly expanded models to hopefully encompass all functionality.

--- models/index.js
+++ models/index.js
@@ -2,6 +2,9 @@ const Game = require('./Game');
 const User = require('./User');
 const UserGame = require('./UserGame');
 const Friend = require('./Friend');
+const Board = require('./Board');
+const Row = require('./Row');
+const Ship = require('./Ship');
 
 Game.belongsToMany(User, {
   through: {
@@ -27,9 +30,68 @@ User.belongsToMany(User, {
   as: 'my_friends'
 });
 
+Game.hasMany(Board, {
+  foreignKey: 'game_id',
+  onDelete: 'CASCADE'
+});
+
+Board.belongsTo(Game, {
+  foreignKey: 'game_id'
+});
+
+User.hasMany(Board, {
+  foreignKey: 'player_id',
+  onDelete: 'CASCADE'
+});
+
+Board.belongsTo(User, {
+  as: 'playerBoard',
+  foreignKey: 'player_id'
+});
+
+User.hasMany(Board, {
+  foreignKey: 'opponent_id',
+  onDelete: 'CASCADE'
+});
+
+Board.belongsTo(User, {
+  as: 'opponentBoard',
+  foreignKey: 'opponent_id'
+});
+
+Board.hasMany(Row, {
+  foreignKey: 'board_id',
+  onDelete: 'CASCADE'
+});
+
+Row.belongsTo(Board, {
+  foreignKey: 'board_id'
+});
+
+Board.hasMany(Ship, {
+  foreignKey: 'board_id',
+  onDelete: 'CASCADE'
+});
+
+Ship.belongsTo(Board, {
+  foreignKey: 'board_id'
+});
+
+User.hasMany(Ship, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Ship.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 module.exports = {
   Game, 
   User,
   UserGame,
-  Friend
+  Friend,
+  Board,
+  Row,
+  Ship
 }
