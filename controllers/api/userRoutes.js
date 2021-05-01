a44bf823c7234cef663a1eec21284fd4a25fea2b
Dgehring <dgehring7@gmail.com>
2021-05-01 13:07:13 -0400

    should have fixed signup and login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,7 +19,7 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = validUsername.id;
             req.session.logged_in = true;
-            res.json({ user: validUsername.username, message: `Welcome ${validUsername.username}`})
+            res.json({ user: validUsername.username, message: `Welcome ${User.username}`})
         });
     } catch (err) {
         res.status(400).json(err);
@@ -40,6 +40,32 @@ router.post('/signup', (req, res) => {
     User.create(req.body)
     .then((newUser) => {
         res.send(newUser);
+    })
+    .then(() => {
+        router.post('/login', async (req, res) => {
+            try {
+                const validUsername = await User.findOne({where: {username: req.body.username},  attributes: { exclude: ['password'] }});
+        
+                if (!validUsername) {
+                    res.status(400).json({ message: 'Password or Username is incorrect, please try again'});
+                    return;
+                }
+        
+                const validPassword = await validUsername.checkPassword(req.body.password);
+                if (!validPassword) {
+                    res.status(400).json({ message: 'Password or Username is incorrect, please try again'});
+                    return;
+                };
+        
+                req.session.save(() => {
+                    req.session.user_id = validUsername.id;
+                    req.session.logged_in = true;
+                    res.json({ user: validUsername.username, message: `Welcome ${validUsername.username}`})
+                });
+            } catch (err) {
+                res.status(400).json(err);
+            }
+        });
     })
     .catch ((err) => {
         res.json(err);
